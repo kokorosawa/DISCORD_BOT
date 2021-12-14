1e23abb0d4d56bd7cf490d13c0bf76892b5c8ce4
kokorosawa <matt920404@gmail.com>
2021-12-15 00:19:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello");
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+
+async def on_ready():
+    print(">>Bot is online<<")
+
+bot.run('OTIwMzM3NDgzMzgyNzg0MDcy.Ybi5Ew.DoyUo_kKxzXQ5AW6ZwWo1HKtlJk')
