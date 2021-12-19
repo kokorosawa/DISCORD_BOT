6797f51f8b19b8c8a6858b7693aa348901005730
kokorosawa <matt920404@gmail.com>
2021-12-19 12:47:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import discord
 from discord.ext import commands
+from discord.flags import Intents
 
-bot = commands.Bot(command_prefix='!')
+intents=discord.Intents.default()
+intents.members=True
+
+bot = commands.Bot(command_prefix='!',intents=intents)
 
 @bot.event
 
 async def on_ready():
     print(">>Bot is online<<")
 
-bot.run('OTIwMzM3NDgzMzgyNzg0MDcy.Ybi5Ew.DoyUo_kKxzXQ5AW6ZwWo1HKtlJk')
+@bot.event
+
+async def on_member_join(member):
+    channel = bot.get_channel(920704186952335420)
+    await channel.send(f'{member}join!')
+
+
+async def on_member_remove(member):
+    channel = bot.get_channel(920704221173674085)
+    await channel.send(f'{member}leave!')
+
+bot.run('OTIwMzM3NDgzMzgyNzg0MDcy.Ybi5Ew.AdKco1B5aV2ZrzQsQBgcZTxRtVQ')
