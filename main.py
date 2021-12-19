4f25882bb8438ad51a6c6e74e4aec9937e8e2a9d
kokorosawa <matt920404@gmail.com>
2021-12-19 15:30:23 +0800

    first verion

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import discord
 from discord.ext import commands
 from discord.flags import Intents
+import  json
+import random
+
+with open("setting.json",'r',encoding= 'utf8') as jfile:
+    jdata = json.load(jfile)
 
 intents=discord.Intents.default()
 intents.members=True
@@ -13,14 +18,27 @@ async def on_ready():
     print(">>Bot is online<<")
 
 @bot.event
-
 async def on_member_join(member):
-    channel = bot.get_channel(920704186952335420)
+    channel = bot.get_channel(int(jdata['welcome_channel']))
     await channel.send(f'{member}join!')
 
-
+@bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(920704221173674085)
+    channel = bot.get_channel(int(jdata['leave_channel']))
     await channel.send(f'{member}leave!')
 
-bot.run('OTIwMzM3NDgzMzgyNzg0MDcy.Ybi5Ew.AdKco1B5aV2ZrzQsQBgcZTxRtVQ')
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)} ms')
+
+@bot.command()
+async def eatap(ctx):
+    res= random.choice(jdata['restaurant_apartment'])
+    await ctx.send(res)
+
+@bot.command()
+async def eatsch(ctx):
+    res= random.choice(jdata['restaurant_school'])
+    await ctx.send(res)
+
+bot.run(jdata["token"])
